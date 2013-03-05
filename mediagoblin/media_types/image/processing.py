956d3038c0bc10e644aeb80c0278b01fd8d3cff0
Gabriel Saldana <gabriel@gabrielsaldana.org>
2013-03-05 00:14:03 -0600

    Adds setting quality=100 to improve image quality when resizing

--- mediagoblin/media_types/image/processing.py
+++ mediagoblin/media_types/image/processing.py
@@ -69,7 +69,7 @@ def resize_image(entry, filename, new_path, exif_tags, workdir, new_size,
     # Copy the new file to the conversion subdir, then remotely.
     tmp_resized_filename = os.path.join(workdir, new_path[-1])
     with file(tmp_resized_filename, 'w') as resized_file:
-        resized.save(resized_file)
+        resized.save(resized_file, quality=100)
     mgg.public_store.copy_local_to_storage(tmp_resized_filename, new_path)
 
 
